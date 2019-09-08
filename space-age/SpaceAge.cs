38095e449e7ae7bda21db1d241b69f7941f52361
Joshua Swick <joshuaswickirl@gmail.com>
2019-09-08 14:56:36 -0600

    Update formatting

--- space-age/SpaceAge.cs
+++ space-age/SpaceAge.cs
@@ -9,12 +9,12 @@ public class SpaceAge
         earthAge = seconds / earthYearInSeconds;
     }
 
-    public double OnEarth() => earthAge / 1.00;
+    public double OnEarth()   => earthAge / 1.00;
     public double OnMercury() => earthAge / 0.2408467;
-    public double OnVenus() => earthAge / 0.61519726;
-    public double OnMars() => earthAge / 1.8808158;
+    public double OnVenus()   => earthAge / 0.61519726;
+    public double OnMars()    => earthAge / 1.8808158;
     public double OnJupiter() => earthAge / 11.862615;
-    public double OnSaturn() => earthAge / 29.447498;
-    public double OnUranus() => earthAge / 84.016846;
+    public double OnSaturn()  => earthAge / 29.447498;
+    public double OnUranus()  => earthAge / 84.016846;
     public double OnNeptune() => earthAge / 164.79132;
 }
