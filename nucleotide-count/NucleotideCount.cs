7490bfaecb078c4616858f4bb468e49096c7c168
Joshua Swick <joshuaswickirl@gmail.com>
2020-07-07 07:17:04 -0600

    Update exercise version and formatting

--- nucleotide-count/NucleotideCount.cs
+++ nucleotide-count/NucleotideCount.cs
@@ -4,21 +4,21 @@ public static class NucleotideCount
 {
     public static IDictionary<char, int> Count(string sequence)
     {
-        var total = new Dictionary<char, int> {
+        var total = new Dictionary<char, int>
+        {
             ['A'] = 0, 
             ['T'] = 0, 
             ['G'] = 0, 
             ['C'] = 0
         };
-
-        foreach (char character in sequence) {
-            if (!total.ContainsKey(character)) {
+        foreach (char character in sequence)
+        {
+            if (!total.ContainsKey(character))
+            {
                 throw new System.ArgumentException("Sequence contains invalid nucleotide.");
             }
-            
             total[character]++;
         }
-        
         return total;
     }
 }
